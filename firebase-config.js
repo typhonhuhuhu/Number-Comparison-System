e627da0912f68a9352e2005f4999043028fadadd
typhonhuhuhu <patrick713huhuhu@gmail.com>
2026-06-16 21:05:52 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB2g_ReT3jWvgD7m3mUMgCiAoDSfupSohw",
+  authDomain: "zhajinhua-game-bfeff.firebaseapp.com",
+  databaseURL: "https://zhajinhua-game-bfeff-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "zhajinhua-game-bfeff",
+  storageBucket: "zhajinhua-game-bfeff.firebasestorage.app",
+  messagingSenderId: "751362513236",
+  appId: "1:751362513236:web:4f92c02586a4f524a1d0c1"
 };
